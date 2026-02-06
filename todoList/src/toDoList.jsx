8fd5c3171dfe00029d6707311b8241e50f434db2
Abhinand k <abhinnanndk@gmail.com>
2026-02-06 12:41:36 +0530

    edit, filter added

--- todoList/src/toDoList.jsx
+++ todoList/src/toDoList.jsx
@@ -1,9 +1,54 @@
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+
 
 function ToDo(){
-    const [tasks, setTask] = useState([]);
+    const [tasks, setTask] = useState(()=>{
+      const savedTasks = localStorage.getItem('tasks');
+      return savedTasks ? JSON.parse(savedTasks):[];
+    });
+
+
     const [newTask, setNewTask] = useState('');
+
+    const [filter, setFilter] = useState(()=>{
+      return localStorage.getItem('filter') || 'all';
+    });
+
+    const [editIndex, setEditIndex] = useState(null);
+    const [editText, setEditText] = useState('');
+
+
+
+    useEffect(()=> {
+      localStorage.setItem('tasks',JSON.stringify(tasks));
+    },[tasks]);
+
+    useEffect(()=>{
+      localStorage.setItem('filter',filter)
+    },[filter]);
+
+
+    function startEdit(index,text){
+      setEditIndex(index);
+      setEditText(text);
+    }
+
+    function saveEdit(index){
+      if(!editText.trim()) return;
+
+      setTask(tasks=> tasks.map((task,i)=>
+      i===index?{...task,text:editText}:task
+      ))
+      setEditIndex(null);
+      setEditText('');
+    };
+
+    const filteredTasks = tasks.filter(task => {
+      if(filter === 'completed') return task.completed;
+      if(filter === 'pending') return !task.completed;
+      return true;
+    })
   
     function deleteTask(index){
         const confirmed = window.confirm(
@@ -12,16 +57,28 @@ function ToDo(){
         if(!confirmed) return;
       setTask(tasks.filter((_, i) => i !== index));
     }
+
+    function cancelEdit() {
+      setEditIndex(null);
+      setEditText('');
+    }
+    
   
     function addTask(){
       if (!newTask.trim()) return; 
-      setTask(t => [...t, newTask]);
+      setTask(t => [...t, {text:newTask, completed:false}]);
       setNewTask('');
+    }
+
+    function toggleTask(index){
+      setTask(tasks=> tasks.map((task,i)=>
+      i===index ? {...task,completed:!task.completed}:task
+      ))
     }
   
     return (
       <div className="todolist">
-        <h1>To-Do List</h1>
+        <h1>Tasks To Do</h1>
   
         <div className="input-group">
           <input
@@ -35,19 +92,68 @@ function ToDo(){
           </button>
         </div>
   
+        <div className='filters'>
+          <button className={filter==='all'?'active':''} 
+                  onClick={()=> setFilter('all')}>
+                       All
+          </button>
+          <button className={filter==='completed'?'active':''} 
+                  onClick={()=> setFilter('completed')}>
+                       Completed
+          </button>
+          <button className={filter==='pending'?'active':''} 
+                  onClick={()=> setFilter('pending')}>
+                       Pending
+          </button>
+        </div>
+          {filteredTasks.length===0?
+            (<p className='fallbackText'>
+              No tasks
+            </p>)
+          :(
         <ol>
-          {tasks.map((task, index) => (
-            <li key={index}>
-              <span>{task}</span>
+          {filteredTasks.map((task, index) => (
+            <li className={task.completed? 'completed':''} key={index}>
+              <label className="task-item">
+                <input
+                  type="checkbox"
+                  checked={task.completed}
+                  disabled={editIndex === index}
+                  onChange={() => toggleTask(index)}
+                />
+               {editIndex === index ? (
+                  <input
+                    className="edit-input"
+                    value={editText}
+                    onChange={(e) => setEditText(e.target.value)}
+                    onKeyDown={(e) => e.key === 'Enter' && saveEdit(index)}
+                    autoFocus
+                  />
+                ) : (
+                  <span>{task.text}</span>
+                )}
+
+              </label>
+              {editIndex === index ? (
+                <>
+                  <button onClick={() => saveEdit(index)}>✔️</button>
+                  <button onClick={cancelEdit}>✖</button>
+                </>
+              ) : (
+                <button onClick={() => startEdit(index, task.text)}>✏️</button>
+              )}
+
               <button
                 className="delete-btn"
+                disabled={editIndex === index}
                 onClick={() => deleteTask(index)}
               >
                 🗑️
               </button>
             </li>
           ))}
-        </ol>
+        </ol>)}
+
       </div>
     );
   }
